d2e8ec2e478bbc2808d141928242c12aec88ec4f
igorskyflyer <igor.dvlpr@gmail.com>
2024-06-19 21:17:26 +0200

    v0.0.6

--- index.mjs
+++ index.mjs
@@ -9,6 +9,31 @@ try {
     versioning: 'auto',
   })
   console.log(aria)
+
+  const token = process.env.GITHUB_TOKEN
+  const octokit = github.getOctokit(token)
+  const context = github.context
+
+  let files
+
+  if (context.eventName === 'push') {
+    const commit = context.payload.head_commit.id
+    const { data } = await octokit.rest.repos.getCommit({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      ref: commit,
+    })
+    files = data.files.map((file) => file.filename)
+  } else if (context.eventName === 'pull_request') {
+    const { data } = await octokit.rest.pulls.listFiles({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      pull_number: context.payload.number,
+    })
+    files = data.map((file) => file.filename)
+
+    console.log(files)
+  }
 } catch (error) {
   setFailed(error.message)
 }
