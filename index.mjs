70cbe143240389755d0bf64c3f558487c02fb54b
igorskyflyer <igor.dvlpr@gmail.com>
2024-06-20 00:46:32 +0200

    v0.0.20

--- index.mjs
+++ index.mjs
@@ -4,14 +4,12 @@ import { Aria } from '@igor.dvlpr/aria/dist/lib/compiler/Aria.mjs'
 import { readFileSync } from 'fs'
 
 try {
-  console.log('Wow')
   const aria = new Aria({
     shouldLog: false,
     versioning: 'auto',
   })
-  console.log(aria)
 
-  const token = process.env.GITHUB_TOKEN
+  const token = process.env.GITHUB_TOKEN || ''
   const octokit = github.getOctokit(token)
   const context = github.context
 
@@ -24,21 +22,17 @@ try {
     ref: commit,
   })
 
-  console.log(data)
+  files = data.files?.map((file) => file.filename)
 
-  files = data.files.map((file) => file.filename)
-  // } else if (context.eventName === 'pull_request') {
-  //   const { data } = await octokit.rest.pulls.listFiles({
-  //     owner: context.repo.owner,
-  //     repo: context.repo.repo,
-  //     pull_number: context.payload.number,
-  //   })
-  //   files = data.map((file) => file.filename)
+  if (!files || files.length === 0) {
+    setFailed('No changed files found.')
+    // @ts-ignore
+    return
+  }
 
   files.forEach((adbt) => {
-    const contents = readFileSync(adbt)
-    console.log(contents.toString())
-    // aria.parseFile(adbt)
+    const contents = readFileSync(adbt, 'utf-8')
+    console.log(aria.parse(contents).nodes)
   })
 } catch (error) {
   setFailed(error.message)
