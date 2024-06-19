2a7633b10ba6327e7552935b09659ccdd1b12737
igorskyflyer <igor.dvlpr@gmail.com>
2024-06-19 20:50:15 +0200

    v0.0.2

--- dist/index.js
+++ dist/index.js
@@ -31082,12 +31082,6 @@ const core = __nccwpck_require__(2186)
 const github = __nccwpck_require__(5438)
 
 try {
-  const nameToGreet = core.getInput('who-to-greet')
-  console.log(`Hello ${nameToGreet}!`)
-  const time = new Date().toTimeString()
-  core.setOutput('time', time)
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`)
 } catch (error) {
   core.setFailed(error.message)
 }
