63201d38e73174fd708f9a99585bed74ef9918a9
igorskyflyer <igor.dvlpr@gmail.com>
2024-06-20 00:48:52 +0200

    v0.0.21

--- index.mjs
+++ index.mjs
@@ -3,35 +3,39 @@ import github from '@actions/github'
 import { Aria } from '@igor.dvlpr/aria/dist/lib/compiler/Aria.mjs'
 import { readFileSync } from 'fs'
 
-try {
-  const aria = new Aria({
-    shouldLog: false,
-    versioning: 'auto',
-  })
+async function action() {
+  try {
+    const aria = new Aria({
+      shouldLog: false,
+      versioning: 'auto',
+    })
 
-  const token = process.env.GITHUB_TOKEN || ''
-  const octokit = github.getOctokit(token)
-  const context = github.context
+    const token = process.env.GITHUB_TOKEN || ''
+    const octokit = github.getOctokit(token)
+    const context = github.context
 
-  let files
+    let files
 
-  const commit = context.payload.head_commit.id
-  const { data } = await octokit.rest.repos.getCommit({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    ref: commit,
-  })
+    const commit = context.payload.head_commit.id
+    const { data } = await octokit.rest.repos.getCommit({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      ref: commit,
+    })
 
-  files = data.files?.map((file) => file.filename)
+    files = data.files?.map((file) => file.filename)
 
-  if (!files || files.length === 0) {
-    setFailed('No changed files found.')
-  } else {
+    if (!files || files.length === 0) {
+      setFailed('No changed files found.')
+      return
+    }
     files.forEach((adbt) => {
       const contents = readFileSync(adbt, 'utf-8')
       console.log(aria.parse(contents).nodes)
     })
+  } catch (error) {
+    setFailed(error.message)
   }
-} catch (error) {
-  setFailed(error.message)
 }
+
+action()
